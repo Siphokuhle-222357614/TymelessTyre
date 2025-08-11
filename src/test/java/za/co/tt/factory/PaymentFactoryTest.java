3c9f3c6f70dd8cb1b4ac1fdf038be014bc494ab2
BonkeBulana <bonkebulana@gmail.com>
2025-08-11 22:58:14 +0200

    I added my Payment and CartItem POJOs, Factories and FactoryTest

---
+++ src/test/java/za/co/tt/factory/PaymentFactoryTest.java
@@ -0,0 +1,28 @@
+/*Author: Bonke Bulana*/
+package za.co.tt.factory;
+
+import org.junit.jupiter.api.Test;
+import za.co.tt.domain.Payment;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+
+class PaymentFactoryTest {
+
+    public static Payment payment1 = PaymentFactory.createPayment(123456, "Credit", "Rejected", 750, LocalDate.of(2025,7,11) , null, null);
+    public static Payment payment2 = PaymentFactory.createPayment(12345, "Debit", "Approved", 345, LocalDate.of(2024,13,12), null, null);
+
+    @Test
+    public void testCreateUserWithAllAttributes() {
+        assertNotNull(payment1);
+        System.out.println(payment1);
+    }
+
+    @Test
+    public void testCreateUserThatFails() {
+        //fail();
+        assertNotNull(payment2);
+        System.out.println(payment2);
+    }
+
+}
