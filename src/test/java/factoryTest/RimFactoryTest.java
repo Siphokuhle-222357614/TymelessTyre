beefde825bb8d275b3947ef7212c678f7c0beeca
Siphokuhle-222357614 <222357614@mycput.ac.za>
2025-08-16 00:02:45 +0200

    Updated classes

--- src/test/java/factoryTest/RimFactoryTest.java
+++ src/test/java/factoryTest/RimFactoryTest.java
@@ -1,4 +1,22 @@
 package factoryTest;
 
+import org.junit.jupiter.api.Test;
+import za.co.tt.domain.Rim;
+import za.co.tt.factory.RimFactory;
+
 public class RimFactoryTest {
+    @Test
+    public void createRimTest() {
+        Rim rim = RimFactory.createRim("18", "Alloy", "Black");
+        System.out.println(rim.toString());
+        assert rim != null;
+    }
+
+    @Test
+    public void createRimWithInvalidSizeTest() {
+        Rim rim = RimFactory.createRim("invalid", "Alloy", "Black");
+        System.out.println(rim.toString());
+        assert rim == null; // Assuming the factory returns null for invalid size
+    }
+
 }
--- src/test/java/factoryTest/TyreFactoryTest.java
+++ src/test/java/factoryTest/TyreFactoryTest.java
@@ -1,4 +1,16 @@
 package factoryTest;
 
+import org.junit.jupiter.api.Test;
+import za.co.tt.domain.Tyre;
+import za.co.tt.factory.TyreFactory;
+
 public class TyreFactoryTest {
+
+    @Test
+    public void testTyreFactory(){
+        TyreFactory tyreFactory = new TyreFactory();
+        Tyre tyre = tyreFactory.createTyre();
+        System.out.println(tyre.toString());
+        assert tyre == null;
+    }
 }
