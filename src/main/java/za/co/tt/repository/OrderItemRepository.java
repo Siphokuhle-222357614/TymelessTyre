e8b5cdb75b132f762f4e6b23485cd88e1a5d2580
ofentselebaka <O.Lebaka@plum.systems>
2025-08-19 13:05:11 +0200

    Order item

---
+++ src/main/java/za/co/tt/repository/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package za.co.tt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.co.tt.domain.OrderItem;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, String> {
+
+}
