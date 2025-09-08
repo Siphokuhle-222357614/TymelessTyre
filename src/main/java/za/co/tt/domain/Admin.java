ee06b186667fc5cfaf0d8623671b6085174b8ca5
ofentselebaka <O.Lebaka@plum.systems>
2025-09-08 17:43:58 +0200

    Final project

---
+++ src/main/java/za/co/tt/domain/Admin.java
@@ -0,0 +1,35 @@
+package za.co.tt.domain;
+
+public class Admin {
+    private String adminId;
+    private String username;
+    private String password;
+
+    public Admin() {}
+
+    public Admin(String adminId, String username, String password) {
+        this.adminId = adminId;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getAdminId() {
+        return adminId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "adminId='" + adminId + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
