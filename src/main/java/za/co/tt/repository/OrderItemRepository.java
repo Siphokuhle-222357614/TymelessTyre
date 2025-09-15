d83c56f3f19ae3eefcc04ca12e7c650cd4dea08a
Yanga Jilaji <222582731@mycput.ac.za>
2025-09-15 02:59:18 +0200

    temporary commented some classes, don't uncomment them.

--- src/main/java/za/co/tt/repository/OrderItemRepository.java
+++ src/main/java/za/co/tt/repository/OrderItemRepository.java
@@ -1,9 +1,9 @@
-package za.co.tt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import za.co.tt.domain.OrderItem;
-
-@Repository
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
+//package za.co.tt.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//import za.co.tt.domain.OrderItem;
+//
+//@Repository
+//public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+//}
--- src/main/java/za/co/tt/service/OrderService.java
+++ src/main/java/za/co/tt/service/OrderService.java
@@ -1,68 +1,68 @@
-package za.co.tt.service;
-
-import za.co.tt.domain.Order;
-
-import java.util.List;
-import java.util.Optional;
-
-public class OrderService implements IOrderService{
-    @Override
-    public Order createOrder(Order order) {
-        return null;
-    }
-
-    @Override
-    public Optional<Order> getOrderById(String id) {
-        return Optional.empty();
-    }
-
-    @Override
-    public List<Order> getAllOrders() {
-        return List.of();
-    }
-
-    @Override
-    public Order updateOrder(Order order) {
-        return null;
-    }
-
-    @Override
-    public void deleteOrder(String id) {
-
-    }
-
-    @Override
-    public void deleteById(String s) {
-
-    }
-
-    @Override
-    public Order read(String s) {
-        return null;
-    }
-
-    @Override
-    public Order save(Order entity) {
-        return null;
-    }
-
-    @Override
-    public Order update(Order entity) {
-        return null;
-    }
-
-    @Override
-    public void deleteById(Long id) {
-
-    }
-
-    @Override
-    public Order read(Long id) {
-        return null;
-    }
-
-    @Override
-    public List<Order> findAll() {
-        return List.of();
-    }
-}
+//package za.co.tt.service;
+//
+//import za.co.tt.domain.Order;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//public class OrderService implements IOrderService{
+//    @Override
+//    public Order createOrder(Order order) {
+//        return null;
+//    }
+//
+//    @Override
+//    public Optional<Order> getOrderById(String id) {
+//        return Optional.empty();
+//    }
+//
+//    @Override
+//    public List<Order> getAllOrders() {
+//        return List.of();
+//    }
+//
+//    @Override
+//    public Order updateOrder(Order order) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void deleteOrder(String id) {
+//
+//    }
+//
+//    @Override
+//    public void deleteById(String s) {
+//
+//    }
+//
+//    @Override
+//    public Order read(String s) {
+//        return null;
+//    }
+//
+//    @Override
+//    public Order save(Order entity) {
+//        return null;
+//    }
+//
+//    @Override
+//    public Order update(Order entity) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void deleteById(Long id) {
+//
+//    }
+//
+//    @Override
+//    public Order read(Long id) {
+//        return null;
+//    }
+//
+//    @Override
+//    public List<Order> findAll() {
+//        return List.of();
+//    }
+//}
